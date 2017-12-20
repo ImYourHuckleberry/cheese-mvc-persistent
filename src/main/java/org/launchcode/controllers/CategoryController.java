d32a33eac738fdafeb276aa05680b83c34ebc5bf
Tyler Krings <w.tyler.krings@gmail.com>
2017-12-20 17:22:46 -0600

    commit 1.3 maybe done

--- src/main/java/org/launchcode/controllers/CategoryController.java
+++ src/main/java/org/launchcode/controllers/CategoryController.java
@@ -3,6 +3,7 @@ package org.launchcode.controllers;
 import org.launchcode.models.Category;
 import org.launchcode.models.data.CategoryDao;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -12,10 +13,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import javax.validation.Valid;
 
 
+@Controller
+@RequestMapping("category")
 public class CategoryController {
+
     @Autowired
     private CategoryDao categoryDao;
-    @RequestMapping(value="")
+
+    @RequestMapping(value = "")
     public String index(Model model) {
 
         model.addAttribute("categories", categoryDao.findAll());
@@ -24,6 +29,7 @@ public class CategoryController {
 
         return "category/index";
     }
+
     @RequestMapping(value = "add", method = RequestMethod.GET)
     public String displayAddForm(Model model) {
         model.addAttribute("title", "Add Category");
@@ -37,7 +43,7 @@ public class CategoryController {
                                  @ModelAttribute @Valid Category category,
                                  Errors errors) {
 
-        if (errors.hasErrors()) {
+        if(errors.hasErrors()){
             return "category/add";
         }
 
@@ -45,5 +51,6 @@ public class CategoryController {
         return ("redirect:");
 
     }
-}
 
+
+}
